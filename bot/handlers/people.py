8907409e74846577410dd24ea953f18529ccf4ee
Tysobot <tysovka.droid@gmail.com>
2021-07-05 11:12:58 +0300

    1

--- bot/handlers/people.py
+++ bot/handlers/people.py
@@ -205,10 +205,10 @@ def people_cb(update, context):
     return END
 
 
-def people_refresh(context, excluded=None):
+def people_refresh(context, excluded=405299021):
     bot = context.bot
     users_rows = get_users()
-    return print(set([row[0] for row in users_rows]))
+    return list(set([row[0] for row in users_rows]) - set(excluded if excluded else []))
     userid_lst = list(set([row[0] for row in users_rows]) - set(excluded if excluded else []))
     for userid in userid_lst:
         try:
